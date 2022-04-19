909496d79e15a6c543ad3579dc327baab46bc7bc
Karilla <bdelay155@gmail.com>
2022-04-20 00:28:39 +0200

    Added some function

--- include/fileUtils.h
+++ include/fileUtils.h
@@ -6,6 +6,20 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+
+/* Table of CRCs of all 8-bit messages. */
+unsigned long crc_table[256];
+   
+/* Flag: has the table been computed? Initially false. */
+int crc_table_computed = 0;
+
+unsigned long update_crc(unsigned long crc, unsigned char *buf,
+                            int len);
+
+unsigned long crc(unsigned char *buf, int len);
+
+void make_crc_table(void);
+
 uint8_t* fileToBuffer(char* fileName);
 
 bool isPNGFile(uint8_t* buffer);
